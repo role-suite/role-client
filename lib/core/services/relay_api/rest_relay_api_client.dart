e126539cf82d4d5e724d57922bfac87bdc6eb699
Altay Taneri <taneri862@gmail.com>
2026-02-20 11:35:19 +0300

    fix(core): serialize REST workspace writes to avoid races

--- lib/core/services/relay_api/rest_relay_api_client.dart
+++ lib/core/services/relay_api/rest_relay_api_client.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:relay/core/models/api_request_model.dart';
 import 'package:relay/core/models/collection_model.dart';
 import 'package:relay/core/models/environment_model.dart';
@@ -11,10 +13,23 @@ class RestRelayApiClient implements RelayApiClient {
   RestRelayApiClient(this._workspace);
 
   final WorkspaceApiClient _workspace;
+  Future<void> _writeQueue = Future.value();
 
   Future<WorkspaceBundle> _getBundle() => _workspace.getWorkspace();
 
   Future<void> _putBundle(WorkspaceBundle bundle) => _workspace.putWorkspace(bundle);
+
+  Future<T> _runWriteLocked<T>(Future<T> Function() action) {
+    final completer = Completer<T>();
+    _writeQueue = _writeQueue.then((_) async {
+      try {
+        completer.complete(await action());
+      } catch (e, st) {
+        completer.completeError(e, st);
+      }
+    });
+    return completer.future;
+  }
 
   @override
   Future<List<CollectionModel>> listCollections() async {
@@ -27,58 +42,70 @@ class RestRelayApiClient implements RelayApiClient {
   @override
   Future<CollectionModel?> getCollection(String id) async {
     final list = await listCollections();
-    try {
-      return list.firstWhere((c) => c.id == id);
-    } catch (_) {
-      return null;
+    for (final collection in list) {
+      if (collection.id == id) {
+        return collection;
+      }
     }
+    return null;
   }
 
   @override
   Future<void> createCollection(CollectionModel collection) async {
-    final bundle = await _getBundle();
-    final newCollections = List<CollectionBundle>.from(bundle.collections)
-      ..add(CollectionBundle(collection: collection, requests: []));
-    await _putBundle(WorkspaceBundle(
-      version: bundle.version,
-      exportedAt: bundle.exportedAt,
-      source: bundle.source,
-      collections: newCollections,
-      environments: bundle.environments,
-    ));
+    return _runWriteLocked(() async {
+      final bundle = await _getBundle();
+      final newCollections = List<CollectionBundle>.from(bundle.collections)..add(CollectionBundle(collection: collection, requests: []));
+      await _putBundle(
+        WorkspaceBundle(
+          version: bundle.version,
+          exportedAt: bundle.exportedAt,
+          source: bundle.source,
+          collections: newCollections,
+          environments: bundle.environments,
+        ),
+      );
+    });
   }
 
   @override
   Future<void> updateCollection(CollectionModel collection) async {
-    final bundle = await _getBundle();
-    final newCollections = <CollectionBundle>[];
-    for (final cb in bundle.collections) {
-      if (cb.collection.id == collection.id) {
-        newCollections.add(CollectionBundle(collection: collection, requests: cb.requests));
-      } else {
-        newCollections.add(cb);
+    return _runWriteLocked(() async {
+      final bundle = await _getBundle();
+      final newCollections = <CollectionBundle>[];
+      for (final cb in bundle.collections) {
+        if (cb.collection.id == collection.id) {
+          newCollections.add(CollectionBundle(collection: collection, requests: cb.requests));
+        } else {
+          newCollections.add(cb);
+        }
       }
-    }
-    await _putBundle(WorkspaceBundle(
-      version: bundle.version,
-      exportedAt: bundle.exportedAt,
-      source: bundle.source,
-      collections: newCollections,
-      environments: bundle.environments,
-    ));
+      await _putBundle(
+        WorkspaceBundle(
+          version: bundle.version,
+          exportedAt: bundle.exportedAt,
+          source: bundle.source,
+          collections: newCollections,
+          environments: bundle.environments,
+        ),
+      );
+    });
   }
 
   @override
   Future<void> deleteCollection(String id) async {
-    final bundle = await _getBundle();
-    final newCollections = bundle.collections.where((cb) => cb.collection.id != id).toList();
-    await _putBundle(WorkspaceBundle(
-      version: bundle.version,
-      exportedAt: bundle.exportedAt,
-      source: bundle.source,
-      collections: newCollections,
-      environments: bundle.environments,
-    ));
+    return _runWriteLocked(() async {
+      final bundle = await _getBundle();
+      final newCollections = bundle.collections.where((cb) => cb.collection.id != id).toList();
+      await _putBundle(
+        WorkspaceBundle(
+          version: bundle.version,
+          exportedAt: bundle.exportedAt,
+          source: bundle.source,
+          collections: newCollections,
+          environments: bundle.environments,
+        ),
+      );
+    });
   }
 
   @override
@@ -90,52 +117,63 @@ class RestRelayApiClient implements RelayApiClient {
   @override
   Future<EnvironmentModel?> getEnvironment(String name) async {
     final list = await listEnvironments();
-    try {
-      return list.firstWhere((e) => e.name == name);
-    } catch (_) {
-      return null;
+    for (final environment in list) {
+      if (environment.name == name) {
+        return environment;
+      }
     }
+    return null;
   }
 
   @override
   Future<void> createEnvironment(EnvironmentModel environment) async {
-    final bundle = await _getBundle();
-    final newEnvs = List<EnvironmentModel>.from(bundle.environments)..add(environment);
-    await _putBundle(WorkspaceBundle(
-      version: bundle.version,
-      exportedAt: bundle.exportedAt,
-      source: bundle.source,
-      collections: bundle.collections,
-      environments: newEnvs,
-    ));
+    return _runWriteLocked(() async {
+      final bundle = await _getBundle();
+      final newEnvs = List<EnvironmentModel>.from(bundle.environments)..add(environment);
+      await _putBundle(
+        WorkspaceBundle(
+          version: bundle.version,
+          exportedAt: bundle.exportedAt,
+          source: bundle.source,
+          collections: bundle.collections,
+          environments: newEnvs,
+        ),
+      );
+    });
   }
 
   @override
   Future<void> updateEnvironment(EnvironmentModel environment) async {
-    final bundle = await _getBundle();
-    final newEnvs = bundle.environments
-        .map((e) => e.name == environment.name ? environment : e)
-        .toList();
-    await _putBundle(WorkspaceBundle(
-      version: bundle.version,
-      exportedAt: bundle.exportedAt,
-      source: bundle.source,
-      collections: bundle.collections,
-      environments: newEnvs,
-    ));
+    return _runWriteLocked(() async {
+      final bundle = await _getBundle();
+      final newEnvs = bundle.environments.map((e) => e.name == environment.name ? environment : e).toList();
+      await _putBundle(
+        WorkspaceBundle(
+          version: bundle.version,
+          exportedAt: bundle.exportedAt,
+          source: bundle.source,
+          collections: bundle.collections,
+          environments: newEnvs,
+        ),
+      );
+    });
   }
 
   @override
   Future<void> deleteEnvironment(String name) async {
-    final bundle = await _getBundle();
-    final newEnvs = bundle.environments.where((e) => e.name != name).toList();
-    await _putBundle(WorkspaceBundle(
-      version: bundle.version,
-      exportedAt: bundle.exportedAt,
-      source: bundle.source,
-      collections: bundle.collections,
-      environments: newEnvs,
-    ));
+    return _runWriteLocked(() async {
+      final bundle = await _getBundle();
+      final newEnvs = bundle.environments.where((e) => e.name != name).toList();
+      await _putBundle(
+        WorkspaceBundle(
+          version: bundle.version,
+          exportedAt: bundle.exportedAt,
+          source: bundle.source,
+          collections: bundle.collections,
+          environments: newEnvs,
+        ),
+      );
+    });
   }
 
   @override
@@ -143,9 +181,7 @@ class RestRelayApiClient implements RelayApiClient {
     final bundle = await _getBundle();
     final cb = bundle.collections.where((b) => b.collection.id == collectionId).toList();
     if (cb.isEmpty) return [];
-    return cb.first.requests
-        .map((r) => r.collectionId == collectionId ? r : r.copyWith(collectionId: collectionId))
-        .toList();
+    return cb.first.requests.map((r) => r.collectionId == collectionId ? r : r.copyWith(collectionId: collectionId)).toList();
   }
 
   @override
@@ -163,91 +199,105 @@ class RestRelayApiClient implements RelayApiClient {
 
   @override
   Future<void> createRequest(ApiRequestModel request) async {
-    final bundle = await _getBundle();
-    final newCollections = <CollectionBundle>[];
-    var added = false;
-    for (final cb in bundle.collections) {
-      if (cb.collection.id != request.collectionId) {
-        newCollections.add(cb);
-        continue;
+    return _runWriteLocked(() async {
+      final bundle = await _getBundle();
+      final newCollections = <CollectionBundle>[];
+      var added = false;
+      for (final cb in bundle.collections) {
+        if (cb.collection.id != request.collectionId) {
+          newCollections.add(cb);
+          continue;
+        }
+        final newRequests = List<ApiRequestModel>.from(cb.requests)..add(request);
+        newCollections.add(CollectionBundle(collection: cb.collection, requests: newRequests));
+        added = true;
       }
-      final newRequests = List<ApiRequestModel>.from(cb.requests)..add(request);
-      newCollections.add(CollectionBundle(collection: cb.collection, requests: newRequests));
-      added = true;
-    }
-    if (!added) {
-      final now = DateTime.now();
-      final placeholder = CollectionModel(
-        id: request.collectionId,
-        name: 'Collection ${request.collectionId}',
-        description: '',
-        createdAt: now,
-        updatedAt: now,
-      );
-      newCollections.add(CollectionBundle(collection: placeholder, requests: [request]));
-    }
-    await _putBundle(WorkspaceBundle(
-      version: bundle.version,
-      exportedAt: bundle.exportedAt,
-      source: bundle.source,
-      collections: newCollections,
-      environments: bundle.environments,
-    ));
-  }
-
-  @override
-  Future<void> updateRequest(ApiRequestModel request) async {
-    final bundle = await _getBundle();
-    final newCollections = <CollectionBundle>[];
-    for (final cb in bundle.collections) {
-      final requests = cb.requests.where((r) => r.id != request.id).toList();
-      if (cb.collection.id == request.collectionId) {
-        requests.add(request);
-      }
-      newCollections.add(CollectionBundle(collection: cb.collection, requests: requests));
-    }
-    if (!newCollections.any((c) => c.collection.id == request.collectionId)) {
-      final now = DateTime.now();
-      newCollections.add(CollectionBundle(
-        collection: CollectionModel(
+      if (!added) {
+        final now = DateTime.now();
+        final placeholder = CollectionModel(
           id: request.collectionId,
           name: 'Collection ${request.collectionId}',
           description: '',
           createdAt: now,
           updatedAt: now,
+        );
+        newCollections.add(CollectionBundle(collection: placeholder, requests: [request]));
+      }
+      await _putBundle(
+        WorkspaceBundle(
+          version: bundle.version,
+          exportedAt: bundle.exportedAt,
+          source: bundle.source,
+          collections: newCollections,
+          environments: bundle.environments,
         ),
-        requests: [request],
-      ));
-    }
-    await _putBundle(WorkspaceBundle(
-      version: bundle.version,
-      exportedAt: bundle.exportedAt,
-      source: bundle.source,
-      collections: newCollections,
-      environments: bundle.environments,
-    ));
+      );
+    });
+  }
+
+  @override
+  Future<void> updateRequest(ApiRequestModel request) async {
+    return _runWriteLocked(() async {
+      final bundle = await _getBundle();
+      final newCollections = <CollectionBundle>[];
+      for (final cb in bundle.collections) {
+        final requests = cb.requests.where((r) => r.id != request.id).toList();
+        if (cb.collection.id == request.collectionId) {
+          requests.add(request);
+        }
+        newCollections.add(CollectionBundle(collection: cb.collection, requests: requests));
+      }
+      if (!newCollections.any((c) => c.collection.id == request.collectionId)) {
+        final now = DateTime.now();
+        newCollections.add(
+          CollectionBundle(
+            collection: CollectionModel(
+              id: request.collectionId,
+              name: 'Collection ${request.collectionId}',
+              description: '',
+              createdAt: now,
+              updatedAt: now,
+            ),
+            requests: [request],
+          ),
+        );
+      }
+      await _putBundle(
+        WorkspaceBundle(
+          version: bundle.version,
+          exportedAt: bundle.exportedAt,
+          source: bundle.source,
+          collections: newCollections,
+          environments: bundle.environments,
+        ),
+      );
+    });
   }
 
   @override
   Future<void> deleteRequest(String requestId) async {
-    final request = await getRequest(requestId);
-    if (request == null) return;
-    final bundle = await _getBundle();
-    final newCollections = <CollectionBundle>[];
-    for (final cb in bundle.collections) {
-      if (cb.collection.id != request.collectionId) {
-        newCollections.add(cb);
-        continue;
+    return _runWriteLocked(() async {
+      final request = await getRequest(requestId);
+      if (request == null) return;
+      final bundle = await _getBundle();
+      final newCollections = <CollectionBundle>[];
+      for (final cb in bundle.collections) {
+        if (cb.collection.id != request.collectionId) {
+          newCollections.add(cb);
+          continue;
+        }
+        final newRequests = cb.requests.where((r) => r.id != requestId).toList();
+        newCollections.add(CollectionBundle(collection: cb.collection, requests: newRequests));
       }
-      final newRequests = cb.requests.where((r) => r.id != requestId).toList();
-      newCollections.add(CollectionBundle(collection: cb.collection, requests: newRequests));
-    }
-    await _putBundle(WorkspaceBundle(
-      version: bundle.version,
-      exportedAt: bundle.exportedAt,
-      source: bundle.source,
-      collections: newCollections,
-      environments: bundle.environments,
-    ));
+      await _putBundle(
+        WorkspaceBundle(
+          version: bundle.version,
+          exportedAt: bundle.exportedAt,
+          source: bundle.source,
+          collections: newCollections,
+          environments: bundle.environments,
+        ),
+      );
+    });
   }
 }
