eb3210730c6834ad8483535134057de6005be350
Altay Taneri <taneri862@gmail.com>
2026-02-27 11:21:50 +0300

    chore(riverpod): add lint tooling and provider test bootstrap

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,27 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widget in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:relay/main.dart';
+import 'package:relay/features/home/presentation/providers/theme_providers.dart';
+import 'test_helpers/pump_app.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  testWidgets('Riverpod scope resolves app providers', (WidgetTester tester) async {
+    SharedPreferences.setMockInitialValues({});
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await pumpAppWithProviders(
+      tester,
+      child: Consumer(
+        builder: (context, ref, _) {
+          final themeMode = ref.watch(themeModeNotifierProvider);
+          return Directionality(textDirection: TextDirection.ltr, child: Text(themeMode.name));
+        },
+      ),
+    );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('system'), findsOneWidget);
   });
 }
